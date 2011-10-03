7d232a5d82ce40fdc6636dc7b12048764b655c7f
Greg Little <glittle@gmail.com>
2011-10-02 22:45:50 -0700

    adds setTimeoutRepeat

--- myutil.js
+++ myutil.js
@@ -10,6 +10,13 @@
 ////////////////////////////
 // misc
 
+function setTimeoutRepeat(func, interval) {
+    setTimeout(function () {
+        func()
+        setTimeoutRepeat(func, interval)
+    }, interval)
+}
+
 function lerp(t0, v0, t1, v1, t) {
     return (t - t0) * (v1 - v0) / (t1 - t0) + v0
 }
