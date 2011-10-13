b04577cc27dde2b1f128ae73777a85520a466226
Greg Little <glittle@gmail.com>
2011-10-13 05:52:21 -0700

    adds csv.js

---
+++ csv.js
@@ -0,0 +1,15 @@
+//
+// public domain
+
+function parseCsv(text) {
+    var a = []
+    var re = /(^|,|\r?\n)("(?:""|.)*"|[^,\r\n]*)/g
+    while (m = re.exec(text)) {
+        if (m[1] != ',') {
+            a.push([])
+        }
+        a[a.length - 1].push(m[2].match(/^"/) ? m[2].replace(/""/g, '"') : m[2])
+    }
+    return a
+}
+
